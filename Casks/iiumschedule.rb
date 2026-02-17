b01983792f75f0fc6a8571e5cda6ac757a27e714
GitHub Action <action@github.com>
2026-02-17 15:31:19 +0000

    :wrench: Update IIUM Schedule cask to v1.4.9+48

--- Casks/iiumschedule.rb
+++ Casks/iiumschedule.rb
@@ -1,6 +1,6 @@
 cask "iiumschedule" do
-  version "1.4.8+47"
-  sha256 "55566666c0b673934c70435a844ddcf68c487997711b954677db9e6903ddec79"
+  version "1.4.9+48"
+  sha256 "c9ef9478c1acd68c7ab304bf17f7cb77a269a69633b0066e3f1a5fe6fd4c2aa0"
 
   url "https://github.com/iiumschedule/iium_schedule/releases/download/#{version}/IIUMSchedule.dmg"
   name "IIUM Schedule"
